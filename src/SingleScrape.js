7b78cb22d1a526122bd452b770bcf6771c16fa6d
Bogosta <bogostamicheal@gmail.com>
2021-09-18 22:33:17 +0300

    added apiflash

--- src/SingleScrape.js
+++ src/SingleScrape.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-import html2canvas from 'html2canvas';
+import DomToImage from 'dom-to-image';
 
 export default function SingleScrape() {
     const [url, setUrl] = useState("");
@@ -9,17 +9,8 @@ export default function SingleScrape() {
 
     useEffect(() => {
         document.getElementById("urlInput").addEventListener("change", (e)=>{
-            fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(e.target.value)}`)
-                .then(response => {
-                    if (response.ok){
-                        return response.json()
-                    }
-                }).then(
-                    (data) => {console.log(data);
-                                setData(data.contents)
-                                document.getElementById("iframe")
-                                .setAttribute("srcDoc", data.contents)}
-                )
+            setUrl(e.target.value)
+            
     })
 })
     return (
@@ -27,15 +18,21 @@ export default function SingleScrape() {
             <input id = "urlInput" placeholder = "https://example.com"></input>
             <label style = {{fontSize:"xx-large"}}>Window View</label>
             <iframe srcDoc = "" id = "iframe"></iframe>
-            <button className = "rounded" onClick = {
-                
+            <button className = "rounded" onClick = {() =>{
 
-            html2canvas(document.querySelector("#capture")).then(canvas => {
-            document.body.appendChild(canvas)
-            })
+                const HTTP = new XMLHttpRequest();
+                const website_url = `https://api.apiflash.com/v1/urltoimage?access_key=3228ac7f29394d2db5a5487ff9390075&format=png&full_page=true&response_type=json&scroll_page=true&url=${encodeURIComponent(url)}`
+                HTTP.open("POST", website_url);
+                HTTP.send();
+                HTTP.onreadystatechange = (e)=> {
+                    console.log(HTTP.response)
+                    document.getElementById("website-image").src =HTTP.response.url
+                    console.log(document.getElementById("website-image").src)
+                }
 
-
+            }
             }>Make screenshot</button>
+            <img src = "" id = "website-image"></img>
         </div>
     )
     }
